9f89e89a3951fc5e6e9f63ff683efec49ff53871
Snare-Hawk <n8scummings@duck.com>
2026-07-22 14:31:03 -0400

    1.6

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,29 +1,3 @@
-const App0 = () => {
-	const course = "Half Stack application development";
-	const part1 = "Fundamentals of React";
-	const exercises1 = 10;
-	const part2 = "Using props to pass data";
-	const exercises2 = 7;
-	const part3 = "State of a component";
-	const exercises3 = 14;
-
-	return (
-		<div>
-			<h1>{course}</h1>
-			<p>
-				{part1} {exercises1}
-			</p>
-			<p>
-				{part2} {exercises2}
-			</p>
-			<p>
-				{part3} {exercises3}
-			</p>
-			<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-		</div>
-	);
-};
-
 const Header = (props) => {
 	return <h1>{props.course}</h1>;
 };
@@ -37,10 +11,17 @@ const Part = (props) => {
 };
 
 const Content = (props) => {
-	const array = props.parts.map((value) => (
-		<Part part={value.name} exercises={value.exercises} key={value.name} />
-	));
-	return <div>{array}</div>;
+	return (
+		<div>
+			{props.parts.map((value) => (
+				<Part
+					part={value.name}
+					exercises={value.exercises}
+					key={value.name}
+				/>
+			))}
+		</div>
+	);
 };
 
 const Total = (props) => {
@@ -54,50 +35,30 @@ const Total = (props) => {
 	);
 };
 
-const App1 = () => {
-	// const-definitions
-	const course = "Half Stack application development";
-	const part1 = "Fundamentals of React";
-	const exercises1 = 10;
-	const part2 = "Using props to pass data";
-	const exercises2 = 7;
-	const part3 = "State of a component";
-	const exercises3 = 14;
-
-	return (
-		<div>
-			<Header course={course} />
-			<Content
-				parts={[part1, part2, part3]}
-				exercises={[exercises1, exercises2, exercises3]}
-			/>
-			<Total exercises={[exercises1, exercises2, exercises3]} />
-		</div>
-	);
-};
-
 const App = () => {
-	const course = "Half Stack application development";
-	const parts = [
-		{
-			name: "Fundamentals of React",
-			exercises: 10,
-		},
-		{
-			name: "Using props to pass data",
-			exercises: 7,
-		},
-		{
-			name: "State of a component",
-			exercises: 14,
-		},
-	];
+	const course = {
+		name: "Half Stack application development",
+		parts: [
+			{
+				name: "Fundamentals of React",
+				exercises: 10,
+			},
+			{
+				name: "Using props to pass data",
+				exercises: 7,
+			},
+			{
+				name: "State of a component",
+				exercises: 14,
+			},
+		],
+	};
 
 	return (
 		<div>
-			<Header course={course} />
-			<Content parts={parts} />
-			<Total parts={parts} />
+			<Header course={course.name} />
+			<Content parts={course.parts} />
+			<Total parts={course.parts} />
 		</div>
 	);
 };
